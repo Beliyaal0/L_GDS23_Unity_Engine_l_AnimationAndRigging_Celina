d8dd9a55c824779ce674736423efe17bc80195ab
Beliyaal0 <celange@stud.macromedia.de>
2025-05-15 14:09:05 +0200

    balls

---
+++ AnimationAndRig/Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    public Collider[] ragdollCollider;
+    public Rigidbody[] ragdollrigidbodies;
+    private Collider mainCollider;
+    private Animator anim;
+    private void Awake()
+    {
+        ragdollCollider = GetComponentsInChildren<Collider>();
+        ragdollrigidbodies = GetComponentsInChildren<Rigidbody>();
+        mainCollider = GetComponent<Collider>();
+        anim = GetComponent<Animator>();
+        DisableRagdoll();
+    }
+    private void DisableRagdoll()
+    {
+        foreach (Collider c in ragdollCollider)
+        {
+            c.enabled = false;
+
+        }
+        foreach (Rigidbody r in ragdollrigidbodies)
+        {
+            r.isKinematic = true;
+        }
+
+        mainCollider.enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().useGravity = true;
+        anim.enabled = true;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            EnableRagdoll();
+        }
+    }
+    public void EnableRagdoll()
+    { 
+        anim.enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+
+        foreach (Collider c in ragdollCollider)
+        {
+            c.enabled = true;
+        }
+        mainCollider.enabled = false;
+        foreach (Rigidbody r in ragdollrigidbodies)
+        {
+            r.isKinematic = false;
+        }
+    }
+}
